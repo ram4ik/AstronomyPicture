2734bb76060d497ae0b837623f71ba08ee5b4a6e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-01-11 17:22:06 +0200

    Initial Commit

---
+++ AstronomyPicture/AstronomyPictureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronomyPictureApp.swift
+//  AstronomyPicture
+//
+//  Created by ramil on 11.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct AstronomyPictureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
